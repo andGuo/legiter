505d03d8a3dbedc9b537adac702b16842c099948
Andrew Guo <andrew.c.guo@gmail.com>
2024-03-17 21:34:25 -0400

    updated keygen to use .Bytes()

--- cmd/keygen.go
+++ cmd/keygen.go
@@ -61,56 +61,32 @@ func keygen(cmd *cobra.Command, args []string) {
 	case "dilithium2":
 		fmt.Println("Generating Dilithium2 key pair")
 		pubKey, privKey := dilithium2.Generate()
-		var packedPubKey [dilithium2.PublicKeySize]byte
-		var packedPrivKey [dilithium2.PrivateKeySize]byte
-		pubKey.Pack(&packedPubKey)
-		privKey.Pack(&packedPrivKey)
-		err := sign.WriteKeyPairToFile(packedPubKey[:], packedPrivKey[:], filename, "dilithium2")
+		err := sign.WriteKeyPairToFile(pubKey.Bytes(), privKey.Bytes(), filename, "dilithium2")
 		checkWriteError(err)
 	case "dilithium3":
 		fmt.Println("Generating Dilithium3 key pair")
 		pubKey, privKey := dilithium3.Generate()
-		var packedPubKey [dilithium3.PublicKeySize]byte
-		var packedPrivKey [dilithium3.PrivateKeySize]byte
-		pubKey.Pack(&packedPubKey)
-		privKey.Pack(&packedPrivKey)
-		err := sign.WriteKeyPairToFile(packedPubKey[:], packedPrivKey[:], filename, "dilithium3")
+		err := sign.WriteKeyPairToFile(pubKey.Bytes(), privKey.Bytes(), filename, "dilithium3")
 		checkWriteError(err)
 	case "dilithium2_aes":
 		fmt.Println("Generating Dilithium2-AES key pair")
 		pubKey, privKey := dilithium2aes.Generate()
-		var packedPubKey [dilithium2aes.PublicKeySize]byte
-		var packedPrivKey [dilithium2aes.PrivateKeySize]byte
-		pubKey.Pack(&packedPubKey)
-		privKey.Pack(&packedPrivKey)
-		err := sign.WriteKeyPairToFile(packedPubKey[:], packedPrivKey[:], filename, "dilithium2_aes")
+		err := sign.WriteKeyPairToFile(pubKey.Bytes(), privKey.Bytes(), filename, "dilithium2_aes")
 		checkWriteError(err)
 	case "dilithium3_aes":
 		fmt.Println("Generating Dilithium3-AES key pair")
 		pubKey, privKey := dilithium3aes.Generate()
-		var packedPubKey [dilithium3aes.PublicKeySize]byte
-		var packedPrivKey [dilithium3aes.PrivateKeySize]byte
-		pubKey.Pack(&packedPubKey)
-		privKey.Pack(&packedPrivKey)
-		err := sign.WriteKeyPairToFile(packedPubKey[:], packedPrivKey[:], filename, "dilithium3_aes")
+		err := sign.WriteKeyPairToFile(pubKey.Bytes(), privKey.Bytes(), filename, "dilithium3_aes")
 		checkWriteError(err)
 	case "ed25519_dilithium2":
 		fmt.Println("Generating Ed25519-Dilithium2 key pair")
 		pubKey, privKey := eddilithium2.Generate()
-		var packedPubKey [eddilithium2.PublicKeySize]byte
-		var packedPrivKey [eddilithium2.PrivateKeySize]byte
-		pubKey.Pack(&packedPubKey)
-		privKey.Pack(&packedPrivKey)
-		err := sign.WriteKeyPairToFile(packedPubKey[:], packedPrivKey[:], filename, "ed25519_dilithium2")
+		err := sign.WriteKeyPairToFile(pubKey.Bytes(), privKey.Bytes(), filename, "ed25519_dilithium2")
 		checkWriteError(err)
 	case "ed448_dilithium3":
 		fmt.Println("Generating Ed448-Dilithium3 key pair")
 		pubKey, privKey := eddilithium3.Generate()
-		var packedPubKey [eddilithium3.PublicKeySize]byte
-		var packedPrivKey [eddilithium3.PrivateKeySize]byte
-		pubKey.Pack(&packedPubKey)
-		privKey.Pack(&packedPrivKey)
-		err := sign.WriteKeyPairToFile(packedPubKey[:], packedPrivKey[:], filename, "ed448_dilithium3")
+		err := sign.WriteKeyPairToFile(pubKey.Bytes(), privKey.Bytes(), filename, "ed448_dilithium3")
 		checkWriteError(err)
 	default:
 		fmt.Println("ERROR - Unsupported algorithm:", algorithm)
@@ -122,6 +98,7 @@ func init() {
 	rootCmd.AddCommand(keygenCmd)
 
 	keygenCmd.Flags().StringVarP(&filename, "filename", "f", "", "The name of the file to write the public/private key pair to")
+	keygenCmd.Flags().StringVarP(&algorithm, "algorithm", "a", "ed448_dilithium3", "The type of digital signature algorithm to use")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
